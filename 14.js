3250b4320d96488f34dfbc366ca6d5e221f79c4f
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-25 23:37:03 +0530

    add

--- 14.js
+++ 14.js
@@ -2,8 +2,8 @@
 
 let age = 17;
 
-if(age>18){
-    console.log("User can play ddlc")
-}else{
-    console.log("User don't play ddlc")
+if(age>18){ //false
+    console.log("User can play ddlc") //skip
+}else{ //true
+    console.log("User don't play ddlc") //write
 }
