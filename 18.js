9f534ef552883a75e9ad882cc12d051277b6c092
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 00:07:37 +0530

    add

---
+++ 18.js
@@ -0,0 +1,19 @@
+// if
+// else if
+// else if
+// else
+
+let tempInDegree = 15;
+if(tempInDegree<0){
+    console.log("extremely cold outside");
+}else if(tempInDegree<16){
+    console.log("It is cold outside!");
+}else if(tempInDegree<25){
+    console.log("Wheather is okey");
+}else if(tempInDegree<35){
+    console.log("lets go for swim");
+}else if(tempInDegree<45){
+    console.log("turn on AC");
+}else{
+    console.log("Too hot!!");
+}
