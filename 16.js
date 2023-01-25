8c9115e4393218952a016bcc0823c7227fb338cb
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-25 23:53:32 +0530

    add

--- 16.js
+++ 16.js
@@ -1,2 +1,15 @@
 // and or operator
 
+let firstName = "Preeti";
+let age = 22;
+// if(firstName[0]==="P"){
+//     console.log("your name starts with P")
+// }
+// if(age>18){
+//     console.log("you are above 18");
+// }
+
+// && operator
+if(firstName[0]==="P" && age>18){
+    console.log("Name starts with P and age above 18")
+}
