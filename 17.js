e59b7226ec61d3edf5afe08771db350ee29a8aa8
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 00:01:31 +0530

    add

---
+++ 17.js
@@ -0,0 +1,11 @@
+// nested if else
+
+// winning number 19
+// 19 your guess is right
+// 17 too low
+// 20 too high
+
+let winningNumber = 19;
+let userGuess = +prompt("Guess a number");
+//prompt always take input in string "12" +12
+console.log(typeof userGuess, userGuess)
