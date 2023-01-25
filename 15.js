8d4688bcaf0b633f24c9306c24c632df5da7d111
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-25 23:50:17 +0530

    add

--- 15.js
+++ 15.js
@@ -1,4 +1,16 @@
 // ternary operator
 
-let age = 15;
-let drink;
+// let age = 15;
+// let drink;
+// if(age>=5){
+//     drink = "coffee";
+// }else{
+//     drink = "milk"
+// }
+// console.log(drink)
+
+// ternary operator / condition operator
+
+let age = 6;
+let drink = age >= 5 ? "coffee" : "milk";
+console.log(drink);
