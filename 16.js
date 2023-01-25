94d18954cbd908b690d43c89a54c74e93d86672c
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-25 23:57:26 +0530

    add

--- 16.js
+++ 16.js
@@ -16,6 +16,8 @@ if(firstName[0]==="P" && age>18){
     console.log("inside else")
 }
 // output inside else
+// when both two condition true print if part.
+// when one conditon fail and one condition pass print else part.
 // || operator
 if(firstName[0]==="P" || age>18){
     console.log("Name starts with P and age above 18")
@@ -23,3 +25,5 @@ if(firstName[0]==="P" || age>18){
     console.log("inside else")
 }
 // output Name starts with P and age above 18
+// when both two condition false print else part.
+// when one conditon fail and one condition pass print if part.
