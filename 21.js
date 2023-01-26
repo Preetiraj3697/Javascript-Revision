2cafcaba0b8d7dfbfb8943d651ba08efbbf280b8
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 21:06:09 +0530

    add

--- 21.js
+++ 21.js
@@ -1,11 +1,15 @@
 //while loop examples
-let num = 10;
+let num = 100;
 // 1+2+3+4+5+6+7+8+9+10
-
+// o(n) time complexity
 let total = 0;
 let i = 0;
-while(i<=10){
+while(i<=num){
     total+=i;
     i++;
 }
 console.log(total);
+
+// Fast o(1) time complexity
+let tol = (num*(num+1)/2);
+console.log(tol)
