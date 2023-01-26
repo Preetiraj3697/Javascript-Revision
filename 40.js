64eee407d98c8c8ed81342dfeea3a9cd409a6bc7
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 23:31:58 +0530

    add

--- 40.js
+++ 40.js
@@ -36,6 +36,13 @@ const obj2 = {
 // const newObj = {...obj2,...obj1};
 // console.log(newObj); // {key4: 'value4', key5: 'value5', key1: 'value1', key2: 'value2'}
 
-const newObj = {...obj2,...obj1,key69:'value69'};
-console.log(newObj);
+// const newObj = {...obj2,...obj1,key69:'value69'};
+// console.log(newObj);
 //{key4: 'value4', key5: 'value5', key1: 'value1', key2: 'value2', key69: 'value69'}key1: "value1"key2: "value2"key4: "value4"key5: "value5"key69: "value69"[[Prototype]]: Object
+
+// const newObj = {..."abc"};
+// console.log(newObj); //{0: 'a', 1: 'b', 2: 'c'}
+
+
+const newObj = {...['item1','item2','item3']};
+console.log(newObj); // {0: 'item1', 1: 'item2', 2: 'item3'}
