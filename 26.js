c7a8199ec5740a4db412bd8349e1ed6489fa9b5d
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 21:25:51 +0530

    add

---
+++ 26.js
@@ -0,0 +1,14 @@
+// intro to arrays
+// how to create arrays
+
+// ordered collection of items
+let fruits = ["apple","mango","grapes"];
+let numbers = [1,2,3,4];
+let mixed = [1,2,3,4,"string",null,undefined,[1,2,3,4]]
+console.log(mixed);
+console.log(numbers)
+console.log(fruits)
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+// array indexing
