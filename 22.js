db870a6cb4394b3b91861b0cfdcffe81336998c9
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 21:10:55 +0530

    add

--- 22.js
+++ 22.js
@@ -1,9 +1,16 @@
 // intro to for loop 
 // print 0 to 9
+// let i = 0;
+// for(let i=0; i<=9; i++){
+//     console.log(i);
+// }
 
-for(let i=0; i<=9; i++){
+// console.log(i);
+//22.js:8 Uncaught ReferenceError: i is not defined
+
+let i = 0;
+for(;i<=9; i++){
     console.log(i);
 }
 
-console.log(i);
-//22.js:8 Uncaught ReferenceError: i is not defined
+console.log("value of i is",i); //value of i is 10
