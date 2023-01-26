37faa8ff97008bd9ed41945e97b672a4be62a958
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 22:47:53 +0530

    add

--- 35.js
+++ 35.js
@@ -1,6 +1,6 @@
 // array destructuring
 
-const myArray = ['value1','value2','value3'];
+// const myArray = ['value1','value2','value3'];
 // const myvar1 = myArray[0];
 // const myvar2 = myArray[1];
 // console.log('value of myvar1',myvar1);
@@ -8,15 +8,40 @@ const myArray = ['value1','value2','value3'];
 // value of myvar1 value1
 // value of myvar2 value2
 //=============================
-let [myvar1, myvar2] = myArray;
+// let [myvar1, myvar2] = myArray;
 
-console.log('value of myvar1',myvar1);
-console.log('value of myvar2',myvar2);
+// console.log('value of myvar1',myvar1);
+// console.log('value of myvar2',myvar2);
 
 // value of myvar1 value1
 // value of myvar2 value2
-myvar1 = 'value changed';
-console.log('value of myvar1',myvar1);
-console.log('value of myvar2',myvar2);
+// myvar1 = 'value changed';
+// console.log('value of myvar1',myvar1);
+// console.log('value of myvar2',myvar2);
 // value of myvar1 value changed
 // value of myvar2 value2
+
+
+const myArray = ['value1','value2','value3','value4','value5'];
+// let [myvar1, , myvar2] = myArray;
+// console.log('value of myvar1',myvar1);
+// console.log('value of myvar2',myvar2);
+// value of myvar1 value1
+// value of myvar2 value3
+// =================
+// let [myvar1, , myvar2] = myArray;
+// let mynewArary = myArray.slice(2);
+// console.log(mynewArary) // value3
+// ======================
+let [myvar1, myvar2, ...myNewArray] = myArray;
+console.log('value of myvar1',myvar1);
+console.log('value of myvar2',myvar2);
+console.log(myNewArray)
+
+// value of myvar1 value1
+// value of myvar2 value2
+// ['value3','value4','value5']
+
+
+
+
