899b2f29e3cf7b2e26fe29a0ac95acd5ea6bb68d
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 21:39:15 +0530

    add

--- 27.js
+++ 27.js
@@ -2,8 +2,30 @@
 // array shift unshift
 
 
+// let fruits = ["apple","mango","grapes"];
+// console.log(fruits);
+// push 
+// fruits.push("banana");
+// console.log(fruits); // ['apple','mango','grapes','banana'];
+
+// pop
+// fruits.pop()
+// let poppedFruits = fruits.pop()
+
+// console.log(fruits.pop()); // grapes
+// console.log(fruits); // ['apple','mango']
+// console.log('poppedFruits is ', poppedFruits); //poppedFruits is  grapes
+
+// unshift
 let fruits = ["apple","mango","grapes"];
 console.log(fruits);
-// push 
-fruits.push("banana");
-console.log(fruits);
+// fruits.unshift("banana");
+// console.log(fruits); //['banana', 'apple', 'mango', 'grapes']
+
+// shift
+// fruits.shift();
+// console.log(fruits) // ['mango', 'grapes']
+
+let removeFruits = fruits.shift();
+console.log(fruits)
+console.log(removeFruits); //apple
