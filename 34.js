a77b70996d972182bd4a88d3f4c8ade7d09cfd71
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 22:36:35 +0530

    add

---
+++ 34.js
@@ -0,0 +1,14 @@
+// for in loop in array
+
+const fruits = ['apple','mango','grapes'];
+for(let fruit in fruits){
+    console.log(fruit); // index
+      //0
+      //1
+      //2
+    console.log(fruits[fruit])
+      //apple
+      //mango
+      //grapes
+  
+}
