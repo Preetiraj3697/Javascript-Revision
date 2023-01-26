092f542e9bae5439e44453b3ace97c3763a577f9
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 22:42:30 +0530

    madd

--- 35.js
+++ 35.js
@@ -1,2 +1,22 @@
 // array destructuring
 
+const myArray = ['value1','value2','value3'];
+// const myvar1 = myArray[0];
+// const myvar2 = myArray[1];
+// console.log('value of myvar1',myvar1);
+// console.log('value of myvar2',myvar2);
+// value of myvar1 value1
+// value of myvar2 value2
+//=============================
+let [myvar1, myvar2] = myArray;
+
+console.log('value of myvar1',myvar1);
+console.log('value of myvar2',myvar2);
+
+// value of myvar1 value1
+// value of myvar2 value2
+myvar1 = 'value changed';
+console.log('value of myvar1',myvar1);
+console.log('value of myvar2',myvar2);
+// value of myvar1 value changed
+// value of myvar2 value2
