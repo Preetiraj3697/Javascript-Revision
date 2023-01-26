3ceb1ac4d96ed8d9e5a24c27ac9a99bce94cc3ab
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 22:27:02 +0530

    add

---
+++ 31.js
@@ -0,0 +1,11 @@
+// use const for creating array
+
+// const pi = 3.14;
+// console.log(pi); // 3.14
+// pi = 12
+// console.log(pi); // Assignment to constant variable
+
+// heap memory me store hai ['apple','mango'] like 0x11,0x12
+const fruits = ['apple','mango'] // 0x11
+fruits.push('banana');
+console.log(fruits);  // ['apple', 'mango', 'banana']
