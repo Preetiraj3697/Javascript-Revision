d25b926374b018f6937b27817ddec3b718a07dbb
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 22:50:22 +0530

    add

---
+++ 36.js
@@ -0,0 +1,14 @@
+// objects
+// reference type
+// array are good but not sufficient
+// for real world data
+// objects store key value pairs
+// object don't have index
+// how to create objects
+const person = {name:"Preeti",age:22}; // object literal
+console.log(person);
+console.log(typeof person);
+
+// how to access data from objects
+
+// hwo to add key value pair to objects
