16b1124744f4ea34c51454b9dbd24735fea8d45b
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 22:28:54 +0530

    add

--- 31.js
+++ 31.js
@@ -6,6 +6,12 @@
 // console.log(pi); // Assignment to constant variable
 
 // heap memory me store hai ['apple','mango'] like 0x11,0x12
+// const fruits = ['apple','mango'] // 0x11
+// fruits.push('banana');
+// console.log(fruits);  // ['apple', 'mango', 'banana']
+
+// use const ================================= because less error
 const fruits = ['apple','mango'] // 0x11
+fruits = ['grapes', 'pineapple'] // Assignment to constant variable
 fruits.push('banana');
-console.log(fruits);  // ['apple', 'mango', 'banana']
+console.log(fruits);
