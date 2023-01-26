970949a41e84d0390b69a6ccd6175919795008e4
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-26 21:45:14 +0530

    add

--- 28.js
+++ 28.js
@@ -1,5 +1,21 @@
 // primitive vs reference data types
+// primitive types // unmuteable
 let num1 = 6;
 let num2 = num1;
-console.log("value is num1 is", num1);
-console.log("value is num2 is ", num2);
+// console.log("value is num1 is", num1); //6
+// console.log("value is num2 is ", num2); //6
+// num1++;
+// console.log("after incrementing num1");
+// console.log("value is num1 is", num1); //7
+// console.log("value is num2 is ", num2); //6
+
+// reference data types
+// array
+let array1 = ['item1','item2'];
+let array2 = array1;
+console.log('value is array1 is ', array1);
+console.log('value is array2 is', array2);
+array1.push('item3');
+console.log('after pushing element to array1')
+console.log('value is array1 is ', array1);
+console.log('value is array2 is', array2);
