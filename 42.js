aca7cc0d6ee50d17033bee757eebde92dc6c6845
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-27 14:48:43 +0530

    add

--- 42.js
+++ 42.js
@@ -11,4 +11,11 @@ const users = [
 
 for(let user of users){
     console.log(user);
+    //{userId: 1, firstName: 'preeti', gender: 'female'}firstName: "preeti"gender: "female"userId: 1[[Prototype]]: Object
+    //{userId: 2, firstName: 'paarthav', gender: 'male'}
+    //{userId: 3, firstName: 'paarth', gender: 'male'}
+    console.log(user.firstName);
+    // preeti
+    // paarthav
+    // paarth
 }
