803c249d60cc6a6f2237ec2c07a236696c6c5a2f
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-27 14:59:18 +0530

    add

---
+++ 44.js
@@ -0,0 +1,21 @@
+// functions
+// like labaour
+
+// console.log("happy birthday to you ......");
+
+// console.log("happy birthday to you........")
+
+function singHappyBirthday(){
+    console.log("happy birthday to you........")
+}
+
+singHappyBirthday(); //calling
+singHappyBirthday();
+singHappyBirthday();
+singHappyBirthday();
+singHappyBirthday();
+
+function twoPlusFour(){
+  console.log(2+4);
+}
+twoPlusFour()
