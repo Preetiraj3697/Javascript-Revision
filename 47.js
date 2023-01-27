83e35edeacd47356216d4bffad80203ee821a572
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-27 15:41:11 +0530

    add

--- 47.js
+++ 47.js
@@ -22,3 +22,15 @@
 //     console.log("hello world")
 // }
 // hello(); //47.js:20 Uncaught TypeError: hello is not a function
+
+// console.log(hello); // undefined
+// var hello = "hello world";
+// console.log(hello); // hello world
+
+// console.log(hello); // 47.js:30 Uncaught ReferenceError: Cannot access 'hello' before initialization
+// let hello = "hello world";
+// console.log(hello); 
+
+console.log(hello); // 47.js:30 Uncaught ReferenceError: Cannot access 'hello' before initialization
+const hello = "hello world";
+console.log(hello); 
