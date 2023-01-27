9a237e689c9664c7fc76e52f74b61e76985fda34
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-27 14:41:29 +0530

    add

--- 41.js
+++ 41.js
@@ -2,12 +2,24 @@
 const band = {
     bandName : 'led zepplin',
     famousSong : 'stairway to heaven',
+    year: 1986,
+    anotherFamousSong: 'kashmir',
 };
 // const bandName = band.bandName;
 // console.log('bandName: ', bandName); //bandName:  led zepplin
 // const famousSong = band.famousSong; //famousSong:  stairway to heaven
 // console.log('famousSong: ', famousSong);
 
-const {bandName, famousSong} = band;
-console.log('famousSong: ', famousSong); // famousSong:  stairway to heaven
-console.log('bandName: ', bandName);  // bandName:  led zepplin
+// const {bandName, famousSong} = band;
+// console.log('famousSong: ', famousSong); // famousSong:  stairway to heaven
+// console.log('bandName: ', bandName);  // bandName:  led zepplin
+
+
+// const {bandName:var1, famousSong} = band;
+// console.log('famousSong: ', famousSong); // famousSong:  stairway to heaven
+// console.log('bandName: ', var1);  // bandName
+
+
+const {bandName, famousSong, ...restProps} = band;
+console.log('restProps: ', restProps); // restProps:  {year: 1986, anotherFamousSong: 'kashmir'}
+
