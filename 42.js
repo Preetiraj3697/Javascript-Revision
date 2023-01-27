8c3772584795f7a909e3f45e80c33ef6567d8f98
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-27 14:44:42 +0530

    add

--- 42.js
+++ 42.js
@@ -2,6 +2,13 @@
 // very useful in real world applications
 
 const users = [
-    {},
-    {},
+    {userId:1, firstName: 'preeti',gender:'female'},
+    {userId:2, firstName: 'paarthav', gender: 'male'},
+    {userId:3, firstName: 'paarth', gender: 'male'},
 ]
+
+// console.log(users);
+
+for(let user of users){
+    console.log(user);
+}
