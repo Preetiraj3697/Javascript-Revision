d5788e2dab98ba316a4ff1947bd6ad53ad9b41c7
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-27 14:37:59 +0530

    add

--- 41.js
+++ 41.js
@@ -1,1 +1,13 @@
 // object destructuring
+const band = {
+    bandName : 'led zepplin',
+    famousSong : 'stairway to heaven',
+};
+// const bandName = band.bandName;
+// console.log('bandName: ', bandName); //bandName:  led zepplin
+// const famousSong = band.famousSong; //famousSong:  stairway to heaven
+// console.log('famousSong: ', famousSong);
+
+const {bandName, famousSong} = band;
+console.log('famousSong: ', famousSong); // famousSong:  stairway to heaven
+console.log('bandName: ', bandName);  // bandName:  led zepplin
