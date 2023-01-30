acb874b9d421267fe8a601f7ec1caa6406fe96fc
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-30 10:42:05 +0530

    add array map method

--- 57.js
+++ 57.js
@@ -1,9 +1,17 @@
-// map method
+// // map method
+
+// const numbers = [3,4,5,6,7,9,8];
+
+// const square = function(number){
+//     return number*number;
+// }
+// const squareNumber = numbers.map(square);
+// console.log('squareNumber: ', squareNumber);  // squareNumber:  (7) [9, 16, 25, 36, 49, 81, 64]
+
 
 const numbers = [3,4,5,6,7,9,8];
 
-const square = function(number){
+const squareNumber = numbers.map((number)=>{
     return number*number;
-}
-const squareNumber = numbers.map(square);
-console.log('squareNumber: ', squareNumber);
+});
+console.log('squareNumber: ', squareNumber);  // squareNumber:  (7) [9, 16, 25, 36, 49, 81, 64]
