773eaee51658810ed94917756008831c0f84af6e
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-30 10:44:07 +0530

    add array map method

--- 57.js
+++ 57.js
@@ -9,9 +9,21 @@
 // console.log('squareNumber: ', squareNumber);  // squareNumber:  (7) [9, 16, 25, 36, 49, 81, 64]
 
 
-const numbers = [3,4,5,6,7,9,8];
+// const numbers = [3,4,5,6,7,9,8];
 
-const squareNumber = numbers.map((number)=>{
-    return number*number;
+// const squareNumber = numbers.map((number)=>{
+//     return number*number;
+// });
+// console.log('squareNumber: ', squareNumber);  // squareNumber:  (7) [9, 16, 25, 36, 49, 81, 64]
+
+const users = [
+    {firstName:"preeti",age:23},
+    {firstName:"paarthav",age:21},
+    {firstName:"anushka",age:17},
+    {firstName:"Babita",age:19},
+]
+
+const userNames = users.map((user)=>{
+    return user.firstName
 });
-console.log('squareNumber: ', squareNumber);  // squareNumber:  (7) [9, 16, 25, 36, 49, 81, 64]
+console.log(userNames)  // (4) ['preeti', 'paarthav', 'anushka', 'Babita']
