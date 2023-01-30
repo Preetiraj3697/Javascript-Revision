973da7a6d9015683356552882304826e923add5f
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-01-30 09:55:44 +0530

    add

--- 51.js
+++ 51.js
@@ -1,1 +1,20 @@
+// default parameters
+// before es6
+// function addTwo(a,b){
+//     if(typeof b === "undefined"){
+//         b=1;
+//     }
+//     if(typeof a === "undefined"){
+//         a=1;
+//     }
+//     return a+b;
+// }
+// const ans = addTwo(4);
+// console.log(ans);
 
+// after es6
+function addTwo(a=1,b=1){
+    return a+b;
+}
+const ans = addTwo(4);
+console.log(ans);
