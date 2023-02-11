3dbb1742e167fe2e35156d19779d7d5162e8b6f6
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-02-11 18:21:03 +0530

    add every function

---
+++ 62.js
@@ -0,0 +1,9 @@
+// every method
+
+const numbers = [2,4,6,12,8,10];
+
+const res = numbers.every((number)=>number%2===0);
+console.log('res: ', res); //res:  true
+
+// callback function ===> true/ false (boolean);
+// every function method ===> true / false (boolean);
