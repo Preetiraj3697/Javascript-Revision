3b95326537f280b2042a167a965886ac86ba8a36
Preeti Raj <107460745+Preetiraj3697@users.noreply.github.com>
2023-02-11 18:25:28 +0530

    add every

--- 62.js
+++ 62.js
@@ -7,3 +7,23 @@ console.log('res: ', res); //res:  true
 
 // callback function ===> true/ false (boolean);
 // every function method ===> true / false (boolean);
+
+// reality example;
+
+// const userCart = [
+//     {productId: 1, productName: "mobile", price:10000},
+//     {productId: 2, productName: "tv", price:15000},
+//     {productId: 3, productName: "laptop", price:21000},
+// ];
+
+// const ans = userCart.every((cartItem)=>cartItem.price<30000);
+// console.log('ans: ', ans); // ans:  true
+
+const userCart = [
+    {productId: 1, productName: "mobile", price:10000},
+    {productId: 2, productName: "tv", price:35000},
+    {productId: 3, productName: "laptop", price:21000},
+];
+
+const ans = userCart.every((cartItem)=>cartItem.price<30000);
+console.log('ans: ', ans); // ans:  false
